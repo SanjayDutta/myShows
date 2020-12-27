bd4244aadd69f42983127f958206c8e503c4a98d
SanjayDutta <sanjay.02.aug.1998@gmail.com>
2020-12-27 22:01:38 +0530

    V1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var contentNames = document.getElementsByClassName("card-title");
+var contentURL = document.getElementsByClassName("value");
 var x = document.getElementsByClassName("card-text");
 for (i in x) {
     var text = x[i].innerHTML;
@@ -11,6 +12,9 @@ for (i in x) {
     }
 }
 
+function redirectToPage() {
+    console.log("Hello");
+}
 
 
 
@@ -22,20 +26,23 @@ for (i in x) {
 
 function searchRecommendation() {
     var userContentValue = document.getElementById("myInput").value;
+
     if (userContentValue.length > 0) {
         let result = [...contentNames].every(myfunction);
-        //console.log(result);
         function myfunction(item, index) {
             let userContentLower = userContentValue.toLowerCase();
             let itemLower = item.innerHTML.toLowerCase();
+            const url = item.getAttribute("value");
+            //console.log(url)
             let result = itemLower.startsWith(userContentLower.slice(0, Math.max(itemLower.length - 1, 1)));
             if (result) {
                 document.getElementById("suggestBox").style.display = "block";
                 document.getElementById("suggestBox").innerHTML = item.innerHTML;
+                document.getElementById("suggestBox").setAttribute("href", url);
+                console.log(document.getElementById("suggestBox"));
                 return false;
             }
             else {
-                //document.getElementById("suggestBox").innerHTML = "Not Available";
                 document.getElementById("suggestBox").style.display = "none";
                 return true;
             }
@@ -43,7 +50,6 @@ function searchRecommendation() {
     }
     else {
         document.getElementById("suggestBox").style.display = "none";
-
     }
 }
 

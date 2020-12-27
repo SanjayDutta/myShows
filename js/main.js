975a40ec3076ea0b59e7338ec3e6806687bfabde
SanjayDutta <sanjay.02.aug.1998@gmail.com>
2020-12-27 13:31:17 +0530

    Search Complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
+var contentNames = document.getElementsByClassName("card-title");
 var x = document.getElementsByClassName("card-text");
-
 for (i in x) {
     var text = x[i].innerHTML;
     if (text) {
@@ -10,3 +10,40 @@ for (i in x) {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+function searchRecommendation() {
+    var userContentValue = document.getElementById("myInput").value;
+    if (userContentValue.length > 0) {
+        let result = [...contentNames].every(myfunction);
+        //console.log(result);
+        function myfunction(item, index) {
+            let userContentLower = userContentValue.toLowerCase();
+            let itemLower = item.innerHTML.toLowerCase();
+            let result = itemLower.startsWith(userContentLower.slice(0, Math.max(itemLower.length - 1, 1)));
+            if (result) {
+                document.getElementById("suggestBox").style.display = "block";
+                document.getElementById("suggestBox").innerHTML = item.innerHTML;
+                return false;
+            }
+            else {
+                //document.getElementById("suggestBox").innerHTML = "Not Available";
+                document.getElementById("suggestBox").style.display = "none";
+                return true;
+            }
+        }
+    }
+    else {
+        document.getElementById("suggestBox").style.display = "none";
+
+    }
+}
+
